67dfce34907fe3b36f0657a7facf65055bf11c14
Kiran <vengatramanvrnfruits75@gmail.com>
2026-02-20 23:52:42 +0530

    alteration

---
+++ js basic/array.4.js
@@ -0,0 +1,14 @@
+cars = ["BMW", "DODGE", "AUDI", "MERCEDES", "ROOLS ROYCE", "MECLERAN"];
+
+cars.push("ford")
+
+console.log(cars)
+
+cars.pop()
+console.log(cars)
+
+cars = ["BMW", "DODGE", "AUDI", "MERCEDES", "ROOLS ROYCE", "MECLERAN"];
+function mapfunction(element, index){
+    console.log(element, index);
+    return index;
+}
