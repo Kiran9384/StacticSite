d90d052b49129ce26c58fd748d27722a35cfc116
Kiran <vengatramanvrnfruits75@gmail.com>
2026-02-24 23:10:15 +0530

    changes

---
+++ js basic/object.js
@@ -0,0 +1,14 @@
+const car={
+    brand:"MERCEDES",
+    model:"MAYBACH",
+    price:250000,
+    color:"black"
+}
+const car2={
+    brand:"MERCEDES",
+    model:"MAYBACH",
+    price:250000,
+    color:"black"
+}
+car.brand="BMW"
+console.log(car)
