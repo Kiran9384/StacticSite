8bc4c3c3c10eaa2c261e0eb26b718f3c80f6879d
Kiran9384 <vengatramanvrnfruits75@gmail.com>
2026-03-13 22:01:51 +0530

    Delete js basic directory

--- js basic/factorial.js
+++
@@ -1,8 +0,0 @@
-function findFactorial (number){
-    let total = 1
-    for(let index = 1; index <= number; index++) {
-        total = total * index
-    }
-     return total
-}
-console.log(findFactorial(4))
